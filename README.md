82c55ccffc18f8d1e96b07d4ea01a04934b28447
Garbolino <orlanmarti@gmail.com>
2021-10-08 10:52:30 +0200

    adds missing files

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# subgraph
+# BOTTO Subgraph
+
 Subgraph for indexing BOTTO and BOTTO-ETH stakes
+
+## Development
+
+### Start docker subgraph node
+
+```
+git clone -n git@github.com:graphprotocol/graph-node.git
+cd graph-node
+git checkout hosted-current
+cd docker
+./setup.sh
+# set ethereum node to `goerli:http://host.docker.internal:9545` for development in graph-node docker-compose.yml
+docker-compose up
+```
+
+### Start truffle development mode
+
+```
+npx truffle develop
+truffle> migrate
+```
+
+### Run & deploy
+
+```
+npm install
+npm run prepare:goerli
+npm run codegen
+npm run create-local
+npm run deploy-local
+```
+
+### Run & deploy goerli
+
+Update `config/goerli.json` with deployed contract addresses.
+
+```
+npm install
+npm run prepare:goerli
+npm run codegen
+npm run deploy-goerli
+```
+
+### Run & deploy mainnet
+
+Update `config/mainnet.json` with deployed contract addresses.
+
+```
+npm install
+npm run prepare:mainnet
+npm run codegen
+npm run deploy-mainnet
+```
+
+### Call functions & emit events
+
+```
+npx truffle develop
+accounts = await web3.eth.getAccounts()
+
+# governance
+botto = await BOTTO.deployed()
+governance = await BottoGovernance.deployed()
+await botto.approve(governance.address, "1000000")
+await governance.stake("1000000")
+await governance.unstake()
+
+# mining
+mining = await BottoLiquidityMining.deployed()
+await botto.transfer(mining.address, "5000000000000")
+await mining.deposit("5000000000000", (await web3.eth.getBlock("latest")).timestamp + 30, (await web3.eth.getBlock("latest")).timestamp + 1000)
+bottoEth = await MockERC20.deployed()
+await bottoEth.approve(mining.address, "2000000")
+await mining.stake("2000000")
+await mining.withdraw()
+```
+
+### Query graph
+
+GraphQL UI: http://localhost:8000/subgraphs/name/botto-subgraph
+
+```
+# example query
+{
+  stakers {
+    id
+    bottoStakes { id, amount, start, end }
+    bottoEthStakes { id, amount, start, end }
+  }
+}
+
+```
+
+### Tear down development
+
+```
+graph-node/docker> docker-compose down -v
+# delete /data folder and rerun ./setup.sh if required to reset network details
+```
---
+++ src/mapping.ts
@@ -0,0 +1,88 @@
+import { log } from "@graphprotocol/graph-ts";
+import {
+  Staked as StakedEvent,
+  Unstaked as UnstakedEvent,
+} from "../generated/BottoGovernance/BottoGovernance";
+import {
+  Stake as StakeEvent,
+  Withdraw as WithdrawEvent,
+} from "../generated/BottoLiquidityMining/BottoLiquidityMining";
+import { BottoEthStake, BottoStake, Staker } from "../generated/schema";
+
+function loadStaker(id: string): Staker {
+  let staker: Staker | null = Staker.load(id);
+  if (staker == null) {
+    staker = new Staker(id);
+  }
+  return <Staker>staker;
+}
+
+// JohnGovernance::Staked event handler
+export function handleStaked(event: StakedEvent): void {
+  let staker = loadStaker(event.params.staker.toHex());
+
+  let stakeId =
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let stake = new BottoStake(stakeId);
+  stake.staker = staker.id;
+  stake.start = event.block.timestamp;
+  stake.amount = event.params.amount;
+  stake.save();
+
+  var currentStakeIds = staker.bottoStakeIds;
+  if (currentStakeIds == null) {
+    currentStakeIds = [];
+  }
+  currentStakeIds.push(stake.id);
+  staker.bottoStakeIds = currentStakeIds;
+  staker.save();
+}
+
+// JohnGovernance::Unstaked event handler
+export function handleUnstaked(event: UnstakedEvent): void {
+  let staker = loadStaker(event.params.staker.toHex());
+  var currentStakeIds = staker.bottoStakeIds;
+  for (var i = 0; i < staker.bottoStakeIds.length; i++) {
+    var stakeId = currentStakeIds.shift();
+    var stake = BottoStake.load(stakeId);
+    if (stake.end == null) {
+      stake.end = event.block.timestamp;
+      stake.save();
+    }
+  }
+}
+
+// JohnLiquidityMining::Stake event handler
+export function handleStake(event: StakeEvent): void {
+  let staker = loadStaker(event.params.staker.toHex());
+
+  let stakeId =
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let stake = new BottoEthStake(stakeId);
+  stake.staker = staker.id;
+  stake.start = event.block.timestamp;
+  stake.amount = event.params.bottoEthIn;
+  stake.save();
+
+  var currentStakeIds = staker.bottoEthStakeIds;
+  if (currentStakeIds == null) {
+    currentStakeIds = [];
+  }
+  currentStakeIds.push(stake.id);
+  staker.bottoEthStakeIds = currentStakeIds;
+  staker.save();
+}
+
+// JohnLiquidityMining::Withdraw event handler
+export function handleWithdraw(event: WithdrawEvent): void {
+  let staker = loadStaker(event.params.staker.toHex());
+  var currentStakeIds = staker.bottoEthStakeIds;
+  for (var i = 0; i < staker.bottoEthStakeIds.length; i++) {
+    var stakeId = currentStakeIds.shift();
+    var stake = BottoEthStake.load(stakeId);
+    if (stake.end == null) {
+      stake.end = event.block.timestamp;
+      stake.save();
+    }
+  }
+}
